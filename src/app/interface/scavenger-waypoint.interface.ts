01559850475e9f46aae615cc2aa39438f432b7f2
Brian Martinson <brian@brianmartinson.com>
2020-10-25 16:44:05 -0700

    Create scavenger-waypoint.interface.ts

---
+++ src/app/interface/scavenger-waypoint.interface.ts
@@ -0,0 +1,32 @@
+export interface IScavengerWaypoint {
+  id: string;
+  name: string;
+  
+  /**
+   * Gathering a waypoint garners some number of points for the
+   * user collecting it. Consider using negative values for
+   * waypoints that are false "wrong answer" waypoints.
+   */
+  value: number;
+  
+  /**
+   * If valid is true, then this waypoint is the correct waypoint
+   * to have gathered during the hunt. If it is set to false, then
+   * this is a waypoint placed as a false "wrong answer" waypoint.
+   */
+  valid: boolean;
+  
+  /**
+   * An array of string messages that will be displayed to the
+   * user for successfully capturing this waypoint. Each message
+   * will be displayed to the user for some duration calculated
+   * based on the length of each message.
+   */
+  dialog: string[];
+  
+  /**
+   * The same concept as dialog, however these messages are
+   * displayed for visiting this waypoint too soon.
+   */
+  outOfOrderDialog: string[];
+}
