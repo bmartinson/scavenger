b94e8fd842951d3c428dfdef06553ee2c218aa6e
Brian Martinson <brian@brianmartinson.com>
2020-10-25 16:55:46 -0700

    Remove Superfluous Property Declaration

--- src/app/interface/scavenger-session.interface.ts
+++ src/app/interface/scavenger-session.interface.ts
@@ -1,7 +1,6 @@
 import { IScavengerModel } from './scavenger-model.interface';
 
 export interface IScavengerSession extends IScavengerModel {
-  id: string;
   user: string;
   idHunt: string;
   active: boolean;
