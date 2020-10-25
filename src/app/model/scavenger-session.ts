ee323b6232a591b8aeebca07a3a16066aa51b595
Brian Martinson <brian@brianmartinson.com>
2020-10-25 16:51:23 -0700

    Create scavenger-session.ts

---
+++ src/app/model/scavenger-session.ts
@@ -0,0 +1,7 @@
+import { IScavengerSession } from '../interface/scavenger-session.interface.ts';
+
+export class ScavengerSession implements IScavengerSession {
+
+  /* * * * * Property Access * * * * */
+
+}
