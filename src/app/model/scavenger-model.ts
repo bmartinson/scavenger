eccb690e60bc29145bd86ac1435a4e9791785624
Brian Martinson <brian@brianmartinson.com>
2020-10-25 17:11:23 -0700

    Create scavenger-model.ts

---
+++ src/app/model/scavenger-model.ts
@@ -0,0 +1,35 @@
+import { IScavengerModel } from '../interface/scavenger-model.interface';
+
+export abstract class ScavengerModel implements IScavengerModel {
+
+  private _id: string;
+
+  /* * * * * Property Access * * * * */
+  
+  /**
+   * The core id for this data model.
+   */
+  public get id(): string {
+    if (!this._id) {
+      const now: Date = new Date();
+      const seed: number = Math.floor(Math.random() * 10001);
+      
+      this._id = `${seed}-${now.getUTCHours()}-${now.getUTCMinutes()}-${now.getUTCSeconds()}-${now.getUTCMilliseconds()}`;
+    }
+  }
+  
+  /* * * * * Core Implementation * * * * */
+  
+  constructor(data?: IScavengerModel) {
+    this._id = data?.id;
+  }
+
+  /**
+   * Get a serialized object representation of this object's data.
+   */
+  public toObject(): IScavengerModel {
+    return {
+      id: this.id;
+    };
+
+}
