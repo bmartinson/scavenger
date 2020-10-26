bd5bda6ce39489f8827608bf981126c5105c9726
Brian Martinson <brian@brianmartinson.com>
2020-10-25 17:17:24 -0700

    Update scavenger-session.ts

--- src/app/model/scavenger-session.ts
+++ src/app/model/scavenger-session.ts
@@ -1,7 +1,42 @@
 import { IScavengerSession } from '../interface/scavenger-session.interface.ts';
+import { ScavengerModel } from './scavenger-model';
 
-export class ScavengerSession implements IScavengerSession {
+export class ScavengerSession extends ScavengerModel implements IScavengerSession {
+  
+  private _user: string;
+  private _idHunt: string;
+  private _active: boolean;
 
   /* * * * * Property Access * * * * */
+
+  public get user(): string {
+    return this._user;
+  }
+
+  public get idHunt(): string {
+    return this._idHunt;
+  }
+
+  public get active(): boolean {
+    return this._active;
+  }
+
+  /* * * * * Core Class Implementation * * * * */
+
+  constructor(data?: IScavengerSession) {
+    super(data);
+
+    this._user = data?.user;
+    this._idHunt = data?.idHunt;
+    this._active = data?.active;
+  }
+    
+  public toObject(): IScavengerSession {
+    return Object.assign(super.toObject(), {
+      user: this.user;
+      idHunt: this.idHunt;
+      active: this.active;
+    });
+  }
 
 }
