544b63a625551a5cddd491514cdc6d7b6b37c89f
bmartinson <brian@brianmartinson.com>
2020-11-24 11:14:15 -0800

    Syntax Fix

--- src/api/user.php
+++ src/api/user.php
@@ -13,116 +13,8 @@ define('ABSPATH', realpath(__DIR__ . '/') . '/');
 require_once(ABSPATH . 'common/api.php');
 require_once(ABSPATH . 'common/definitions.php');
 
-session_start();
-
 // defines the name of this api endpoint
 $endpoint = "user";
-
-// function apiMain($database, &$response)
-// {
-//   global $endpoint;
-//   global $client;
-//   global $apiKey;
-
-//   // if we have the token check our database
-//   if (is_array($token)) {
-//     // check to see if the user exists in the g2pdf user table
-//     $fetchGUser = &ExecuteSQL(
-//       $database,
-//       sprintf("SELECT id, email, authToken FROM g2pdf_User WHERE email=%s", GetSQLValueStringi($database, $userData['email'], "text"))
-//     );
-
-//     if ($fetchGUser->num_rows == 0) {
-//       // no g2pdf user exists, create one
-//       ExecuteSQL(
-//         $database,
-//         sprintf(
-//           "INSERT INTO g2pdf_User (email, authToken, created) VALUES (%s, %s, NOW())",
-//           GetSQLValueStringi($database, $userData['email'], "text"),
-//           GetSQLValueStringi($database, $encodedToken, "text")
-//         )
-//       );
-//     } else {
-//       // update the existing g2pdf user with the new auth token and email
-//       while ($row_GUser = $fetchGUser->fetch_object()) {
-//         ExecuteSQL(
-//           $database,
-//           sprintf(
-//             "UPDATE g2pdf_User SET email=%s, authToken=%s, modified=NOW() WHERE id=%s",
-//             GetSQLValueStringi($database, $userData['email'], "text"),
-//             GetSQLValueStringi($database, $encodedToken, "text"),
-//             GetSQLValueStringi($database, $row_GUser->id, "int")
-//           )
-//         );
-//       }
-//     }
-
-//     $fetchGUser->close();
-
-//     // check for the user entry in the core database table (User)
-//     $fetchUser = &ExecuteSQL(
-//       $database,
-//       sprintf("SELECT id, firstName, appPermissions FROM User WHERE email=%s", GetSQLValueStringi($database, $userData['email'], "text"))
-//     );
-
-//     if ($fetchUser->num_rows == 0) {
-//       // no core user exists, create one
-//       ExecuteSQL(
-//         $database,
-//         sprintf(
-//           "INSERT INTO User (firstName, lastName, email, appPermissions, created) VALUES (%s, %s, %s, %u, NOW())",
-//           GetSQLValueStringi($database, $userData['givenName'], "text"),
-//           GetSQLValueStringi($database, $userData['familyName'], "text"),
-//           GetSQLValueStringi($database, $userData['email'], "text"),
-//           GetSQLValueStringi($database, app_permission_g2pdf, "int")
-//         )
-//       );
-
-//       $fetchUser->close();
-//     } else {
-//       // update the existing core user with the new data
-//       while ($row_User = $fetchUser->fetch_object()) {
-//         ExecuteSQL(
-//           $database,
-//           sprintf(
-//             "UPDATE User SET email=%s, appPermissions=%u, modified=NOW() WHERE id=%s",
-//             GetSQLValueStringi($database, $userData['email'], "text"),
-//             GetSQLValueStringi($database, $row_User->appPermissions | app_permission_g2pdf, "int"),
-//             GetSQLValueStringi($database, $row_User->id, "int")
-//           )
-//         );
-//       }
-//       $fetchUser->close();
-
-//       // check for the user entry in the core database table (User) for the row we just added
-//       $fetchUser = &ExecuteSQL(
-//         $database,
-//         sprintf("SELECT id, firstName FROM User WHERE email=%s", GetSQLValueStringi($database, $userData['email'], "text"))
-//       );
-//       $row_User = $fetchUser->fetch_object();
-//     }
-
-//     // generate an auth token for our core api services
-//     generateAuthToken($row_User->id, $row_User->firstName, $apiKey, true);
-
-//     // store a g2pdf_access flag in the cookie
-//     $expirationTime = time() + expiration_days;
-//     setcookie('g2pdf_access', 1, $expirationTime, '/');
-
-//     // redirect to g2pdf
-//     header('Location: ' . "https://www.brianmartinson.com/g2pdf/");
-//   } else {
-//     // if the g2pdf_access flag was set, make sure it is clear
-//     if (isset($_COOKIE['g2pdf_access'])) {
-//       unset($_COOKIE['g2pdf_access']);
-//     }
-
-//     // redirect to g2pdf
-//     header('Location: ' . "https://www.brianmartinson.com/g2pdf/");
-//   }
-// }
-
-// parse the _REQUEST parameters and execute the endpoint
 
 function getUser($database, &$response, $id)
 {
@@ -175,9 +67,10 @@ function createUser($database, &$response, $data)
       throw new Exception('Last name is required', errorno_param_missing);
     }
 
-    &ExecuteSQL(
+    ExecuteSQL(
       $database,
-      sprintf("INSERT INTO User (email, firstName, lastName, created) VALUES (%s, %s, %s, NOW())",
+      sprintf(
+        "INSERT INTO User (email, firstName, lastName, created) VALUES (%s, %s, %s, NOW())",
         GetSQLValueStringi($database, $data->email, "text"),
         GetSQLValueStringi($database, $data->firstName, "text"),
         GetSQLValueStringi($database, $data->lastName, "text")
@@ -206,9 +99,10 @@ function createUser($database, &$response, $data)
         ]);
       }
 
-    // close the connection and return with the users that have already been created
-    $fetchUser->close();
-    return;
+      // close the connection and return with the users that have already been created
+      $fetchUser->close();
+      return;
+    }
   }
 
   // close the connection
