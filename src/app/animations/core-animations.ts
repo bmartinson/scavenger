957d29d2451711a51a54f552a32b8aaeb53d2997
bmartinson <brian@brianmartinson.com>
2021-04-06 22:05:10 -0700

    Remove Delayed Drop Down Animations

--- src/app/animations/core-animations.ts
+++ src/app/animations/core-animations.ts
@@ -20,26 +20,6 @@ export const dropDownAnimation = trigger(
   ],
 );
 
-export const delayedDropDownAnimation = trigger(
-  'delayedDropDownAnimation',
-  [
-    transition(
-      ':enter',
-      [
-        style({ top: -100, position: 'absolute' }),
-        animate('.5s .25s ease-out', style({ top: 0, position: 'absolute' })),
-      ],
-    ),
-    transition(
-      ':leave',
-      [
-        style({ top: '0' }),
-        animate('.5s .25s ease-in', style({ top: -100, position: 'absolute' })),
-      ],
-    ),
-  ],
-);
-
 export const fadeInOutQuickAnimation = trigger(
   'fadeInOutQuickAnimation',
   [
